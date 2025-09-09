7cc7a5414221374ae660e2b53bc0a58aa15a3eb0
ahmad-khatib0 <ahmadkhatib923@gmail.com>
2025-09-09 23:04:34 +0300

    add return messages

--- src/controller/extension.rs
+++ src/controller/extension.rs
@@ -1,6 +1,9 @@
 use megacommerce_proto::google::rpc::Status;
 use megacommerce_proto::service::auth::v3::CheckResponse;
+use megacommerce_shared::models::translate::tr;
 use tonic::Code;
+
+use super::Controller;
 
 pub trait CheckResponseExt {
   fn ok() -> Self;
@@ -24,5 +27,24 @@ impl CheckResponseExt for CheckResponse {
       }),
       ..Default::default()
     }
+  }
+}
+
+impl Controller {
+  pub fn not_found_msg(lang: &str) -> String {
+    return tr::<String>(lang, "error.not_found", None)
+      .unwrap_or("The requested path is not provided!".into());
+  }
+
+  pub fn invalid_token_msg(lang: &str) -> String {
+    return tr::<String>(lang, "jwt.payload.invalid", None)
+      .unwrap_or("Sorry, the authentication payload is invalid, please login first".into());
+  }
+
+  pub fn int_err_msg(lang: &str) -> String {
+    return tr::<String>(lang, "error.internal", None).unwrap_or(
+      "Sorry, Unexpected internal server error. Our team has been notified. Please try again"
+        .into(),
+    );
   }
 }
