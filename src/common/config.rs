e57e3e079b07533979ecfabb01bf216d9e7f3558
ahmad-khatib0 <ahmadkhatib923@gmail.com>
2025-12-17 13:38:33 +0300

    load config based on ENV

--- src/common/config.rs
+++ src/common/config.rs
@@ -1,6 +1,6 @@
 use std::{error::Error, sync::Arc, time::Duration};
 
-use megacommerce_proto::{config_get_response::Response, Config as SharedConfig, ConfigGetRequest};
+use megacommerce_proto::{config_get_response::Response, Environment, Config as SharedConfig, ConfigGetRequest};
 use megacommerce_shared::models::{
   context::Context,
   errors::{app_error_from_proto_app_error, BoxedErr, ErrorType, InternalError},
@@ -11,6 +11,16 @@ use tonic::Request;
 use super::Common;
 
 impl Common {
+  fn get_service_env(&self) -> i32 {
+    let env = self.service_config.service.env.as_str();
+    match env {
+      "local" => Environment::Local as i32,
+      "dev" => Environment::Dev as i32,
+      "production" => Environment::Production as i32,
+      _ => Environment::Dev as i32,
+    }
+  }
+
   pub(super) async fn config_get(&mut self) -> Result<SharedConfig, BoxedErr> {
     let err_msg = "failed to get configurations from common service";
     let mk_err = |msg: &str, err: Box<dyn Error + Send + Sync>| {
@@ -23,7 +33,8 @@ impl Common {
       })
     };
 
-    let req = Request::new(ConfigGetRequest {});
+    let env = self.get_service_env();
+    let req = Request::new(ConfigGetRequest { env });
     let res = timeout(Duration::from_secs(5), self.client().unwrap().config_get(req)).await;
 
     match res {
